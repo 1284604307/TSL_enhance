e0ffe0dc1dc21765157840606492353547d50ffe
17564500258 <1284604307@qq.com>
2025-03-19 18:55:02 +0800

    新脚本

---
+++ scripts/myscripts/linux/LSTM_比利时_96_1.sh
@@ -0,0 +1,41 @@
+export CUDA_VISIBLE_DEVICES=1
+
+task_name=base
+model_name=LSTM
+ignore_columns="Decremental bid Indicator,Region,Grid connection type,Resolution code,Offshore/onshore"
+target="Measured & Upscaled"
+result_rpath=/kaggle/working
+root_path=/kaggle/input/all-data
+data_path=20221201_20241201_Federal_utc.csv
+date_column=Datetime
+data=former
+
+python -u run.py \
+--task_name $task_name \
+--loss MSE \
+--scale 1 \
+--is_training 1 \
+--root_path $root_path \
+--model $model_name \
+--data base \
+--features MS \
+--e_layers 2 \
+--d_layers 1 \
+--factor 6 \
+--enc_in 15 \
+--dec_in 15 \
+--c_out 1 \
+--batch_size 64 \
+--d_model 16 \
+--des Exp \
+--learning_rate 0.001 \
+--date_column $date_column \
+--target "$target" \
+--ignore_columns "$ignore_columns" \
+--result_rpath $result_rpath \
+--data_path $data_path \
+--seq_len 96 \
+--label_len 1 \
+--pred_len 1 \
+--num_workers 1 \
+--train_epochs 30
